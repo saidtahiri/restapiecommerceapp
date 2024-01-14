035deae86c0bafe1442a0c70fdc53ad5fe51671c
Tahiri Said <said.tahiri@hotmail.com>
2024-01-14 21:12:57 +0100

    added Category and Order Modules

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const CategorySchema= new mongoose.Schema({
+    title:{
+        type:String,
+        trim:true,
+        required:[true,'Please enter the title']
+    },
+    description:{
+        type:String,
+        required:false,
+
+    }
+
+});
+module.exports = mongoose.model('category', CategorySchema)
